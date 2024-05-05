344cd83fcd22b02fe01d6fe671072e556d8f26b7
MisterX459 <s28229@pjwstk.edu.pl>
2024-05-05 15:46:20 +0200

    Final

---
+++ APBD6/AddProductToWarehouse.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace APBD6;
+
+public class AddProductToWarehouse
+{
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    public int IdWarehouse { get; set; }
+    
+    [Range(1,Int32.MaxValue)]
+    public int Amount { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+
+}
